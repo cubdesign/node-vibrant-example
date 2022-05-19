746ec60d5678b13d5632761a3d90095926dcea3f
cubdesign <takeo@cubdesign.com>
2022-05-20 03:17:39 +0900

    next.jsセットアップ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+/*
+
+import Vibrant from "node-vibrant";
+import path from "path";
+
+const image1: string = path.resolve(
+  __dirname,
+  "../images/david-clode-fT2qXggBlks-unsplash.jpg"
+);
+
+// Using builder
+Vibrant.from(image1).getPalette((err, palette) => console.log(palette));
+
+
+
+*/
 
 const Home: NextPage = () => {
   return (
@@ -18,7 +35,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +76,11 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by cubdesign
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
