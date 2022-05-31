37ef934333b6760d90c934a4262c3d437a7bcf59
cubdesign <takeo@cubdesign.com>
2022-06-01 01:23:05 +0900

    add Test

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,11 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import PlaygroundPage from "@/pages/index";
 
-describe("PlaygroundPage", () => {
-  it("renders a heading", () => {
-    const origin: string = "http://localhost:3000";
+const origin: string = "http://localhost:3000";
 
+describe("PlaygroundPage", () => {
+  beforeEach(() => {
+    URL.createObjectURL = jest.fn();
+  });
+
+  test("renders a heading", async () => {
     render(<PlaygroundPage origin={origin} />);
     expect(screen.getByText(/Playground/)).toBeInTheDocument();
+  });
+
+  test.skip("結果表示できる", async () => {
+    // ↓ ここで落ちる
+    const Muted = await waitFor(() => screen.findByText(/Muted/));
+    expect(Muted).toBeInTheDocument();
+  });
+
+  afterEach(() => {
+    // @ts-ignore: URL.createObjectURL is mocked within beforeEach()
+    URL.createObjectURL.mockReset();
+    jest.restoreAllMocks();
   });
 });
